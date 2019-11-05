bb2b82affb8301c5590c4bf81808d3b6220607a0
Prashant SG <prashantshg@kpit.com>
2019-11-05 11:56:51 +0530

    Txformer converts basic data

--- txformersys/src/simple.cpp
+++ txformersys/src/simple.cpp
@@ -4,25 +4,57 @@
 #include <mutex>
 #include <condition_variable>
 #include <utility>
+#include <iterator>
 #include <thread>
 #include <chrono>
 #include <queue>
 #include <future>
 
-struct SourceData {
-    size_t i;
-    std::string s;
-};
+namespace Consumer {
+    struct Data {
+        size_t i;
+        std::string s;
+    };
+    std::mutex cqMutex;
+    std::queue<Consumer::Data> consumerQ;
+    bool consume( const std::vector<Consumer::Data> & in ) {
+        std::lock_guard<std::mutex> lock( cqMutex );
+        for( auto & i : in ) { consumerQ.push( i ); }
+        return true;
+    }
+}
 
-size_t TOTAL_ITEMS = 10;
+namespace Producer {
+    struct Data {
+        size_t i;
+        std::string s;
+    };
+    std::mutex pqMutex;
+    std::queue<Producer::Data> producerQ;
+    std::vector<Producer::Data> produce() {
+        std::vector<Producer::Data> output;
+        std::lock_guard<std::mutex> lock( pqMutex );
+        while( ! producerQ.empty() ) {
+            output.push_back( producerQ.front() );
+            producerQ.pop();
+        }
+        return output;
+    }
+}
 
-struct UserData {
-    size_t i;
-    std::string s;
-};
-
-std::queue<SourceData> gq;
-std::mutex gqMutex;
+namespace Txformer {
+    Consumer::Data convert( const Producer::Data & data ) {
+        return { data.i, data.s };
+    }
+    template <template <typename> class C>
+    C<Consumer::Data> convert( const C<Producer::Data> & in ) {
+        C<Consumer::Data> out;
+        for( auto & i : in ) {
+            out.push_back( convert( i ) );
+        }
+        return out;
+    }
+}
 
 namespace System {
     class ExitSignal {
@@ -40,10 +72,11 @@ namespace System {
             }
         }
     };
-    class Application {
+    class Stepper {
         public:
         virtual bool step() = 0;
     };
+    class Application : public Stepper { };
     template <class APP> class Runner {
         std::unique_ptr<APP> app;
 
@@ -79,60 +112,60 @@ namespace System {
     };
 }
 
-namespace User {
+namespace Consumer {
     class Application : public System::Application {
-        std::vector<SourceData> data;
         public:
         bool step() override {
             {
-                std::lock_guard<std::mutex> lock( gqMutex );
-                data.push_back( gq.front( ) );
-                gq.pop();
+                std::lock_guard<std::mutex> lock( cqMutex );
+                if( ! consumerQ.empty() ) {
+                    auto data = consumerQ.front( );
+                    std::cout << "Consumer::Step: received:" << data.i << "\n";
+                    consumerQ.pop();
+                }
             }
-            std::cout << "User::Step: received:" << data.size() << "\n";
-            if( data.size() == TOTAL_ITEMS ) return false;
             return true;
         }
     };
 }
 
-namespace Source {
+namespace Producer {
     class Application : public System::Application {
-        std::vector<SourceData> dataArray;
-        size_t counter = 0;
+        std::queue<Data> queue;
+        size_t counter{0};
         public:
-        Application() {
-            for( size_t i = 0 ; i < TOTAL_ITEMS ; i ++ ) {
-                dataArray.push_back( { i, std::string("src.data.") + std::to_string(i) } );
-            }
-        }
         bool step() override {
-            std::cout << "Source::Step\n";
-            {
-                std::lock_guard<std::mutex> lock( gqMutex );
-                gq.push( dataArray[counter++] );
-            }
-            if( counter == TOTAL_ITEMS ) return false;
+            std::cout << "Producer::Step\n";
+            producerQ.push( {counter++, "ProducerData"} );
             return true;
+        }
+    };
+}
+
+namespace Txformer {
+    class Application : public System::Application {
+        public:
+        bool step() {
+            return Consumer::consume( Txformer::convert( Producer::produce() ) );
         }
     };
 }
 
 int main()
 {
-    System::Runner< Source::Application >       runnerForSource;
-    System::Runner< Source::Application >       runnerForXformer;
-    System::Runner< User::Application >         runnerForUser;
+    System::Runner< Producer::Application > producerRunner{};
+    System::Runner< Txformer::Application > txformerRunner{};
+    System::Runner< Consumer::Application > consumerRunner{};
 
-    runnerForXformer.start();
-    runnerForUser.start();
-    runnerForSource.start();
+    producerRunner.start();
+    txformerRunner.start();
+    consumerRunner.start();
 
     std::this_thread::sleep_for(std::chrono::seconds(5));
  
-    runnerForSource.stop();
-    runnerForUser.stop();
-    runnerForXformer.stop();
+    consumerRunner.stop();
+    txformerRunner.stop();
+    producerRunner.stop();
 
     std::cout << "Exiting...\n";
 }
